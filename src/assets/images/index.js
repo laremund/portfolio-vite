012c7f31be8b5ec6c6a0bc00ad8a97826be049de
laremund <larsgremund@gmail.com>
2025-03-24 18:42:03 -0600

    feature(lightbox): Added Kierkegaard's Corner to project lightbox slideshow

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import checklistAppImage from "./checklist_app.png";
 import spotifyAppImage from "./spotify_thing.png";
-import myResumeSite from "./my_resume_site.png"
+import myResumeSite from "./my_resume_site.png";
+import philosophySite from "./philosophy_site.png";
 
-export { checklistAppImage, spotifyAppImage, myResumeSite };
+export { checklistAppImage, spotifyAppImage, myResumeSite, philosophySite };
