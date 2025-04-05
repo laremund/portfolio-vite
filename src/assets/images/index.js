50438e431006879eb2105f4a8d5ebc63f6fa6d66
laremund <larsgremund@gmail.com>
2025-04-04 18:54:12 -0600

    style(main-grid-section, lightboxes): Modified main-grid-section to include another section to showcase finished projects. Messed with a lot of App.css and LightboxSlideshow.css, there's some things that need to be organized/standardized in there, maybe some unnecessary lines

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,7 @@ import checklistAppImage from "./checklist_app.png";
 import spotifyAppImage from "./spotify_thing.png";
 import myResumeSite from "./my_resume_site.png";
 import philosophySite from "./philosophy_site.png";
+import artShareLA from "./art_share_la.png";
+import jjkKillBill from "./jjk_kill_bill.png"
 
-export { checklistAppImage, spotifyAppImage, myResumeSite, philosophySite };
+export { checklistAppImage, spotifyAppImage, myResumeSite, philosophySite, artShareLA, jjkKillBill };
